3e2c9a33f4bcee96fce22ed09e626eb8d79af3c2
Galleazzo <paulogalleazzo1@gmail.com>
2024-05-29 14:57:03 -0300

    primeiro commit

---
+++ Banco/src/main/java/br/com/ibm/desafio/banco/BancoIbmApplication.java
@@ -0,0 +1,14 @@
+package br.com.ibm.desafio.banco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoIbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoIbmApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ Banco/src/main/java/br/com/ibm/desafio/banco/model/Cliente.java
@@ -0,0 +1,4 @@
+package br.com.ibm.desafio.banco.model;
+
+public class Cliente {
+}
